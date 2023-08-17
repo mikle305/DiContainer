89f393b3b6453f3ee9ebc5f9dfe5746c66fa43cb
mikle305 <mikle3305@gmail.com>
2023-08-17 12:56:37 +0300

    Fixed Inject member "is never used" in jetbrains ide

--- UniDependencyInjection/Unity/InjectAttribute.cs
+++ UniDependencyInjection/Unity/InjectAttribute.cs
@@ -2,7 +2,15 @@
 
 namespace UniDependencyInjection.Unity
 {
-    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property)]
+#if UNITY_2018_4_OR_NEWER
+    [JetBrains.Annotations.MeansImplicitUse(
+        JetBrains.Annotations.ImplicitUseKindFlags.Access |
+        JetBrains.Annotations.ImplicitUseKindFlags.Assign |
+        JetBrains.Annotations.ImplicitUseKindFlags.InstantiatedNoFixedConstructorSignature)]
+#endif
+    [AttributeUsage(validOn: 
+        AttributeTargets.Method | 
+        AttributeTargets.Property)]
     public class InjectAttribute : Attribute
     {
     }
